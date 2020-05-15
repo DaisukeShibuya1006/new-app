6eda94d75925b15d2a4d74324831cff8cf50a7bb
DaisukeShibuya1006 <dshibuya1006@gmail.com>
2020-05-15 19:28:57 +0900

    config.assets.initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,8 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+config.assets.initialize_on_precompile = false
+
 
 module NewApp
   class Application < Rails::Application
